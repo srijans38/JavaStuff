9128e4ea5f1f4989260dbcc984bd89c6505405d6
jermgaming <atinder999@gmail.com>
2019-11-02 19:03:15 +0530

    Abstract Class

---
+++ 5/Area.java
@@ -0,0 +1,62 @@
+abstract class Dimension
+{
+
+    int l , b ;
+    int dis,base;
+
+}
+
+class Rectangle extends Dimension
+{   int area1;
+   Rectangle(int x,int y)
+   {
+       l=x;
+       b=y;
+   }
+  void areaRectangle()
+  {
+      
+      area1=l*b;
+  }
+  void displayRectanglearea()
+  {
+      System.out.println("Rectangle area " +area1);
+  }
+
+
+}
+
+
+class Triangle extends Dimension
+{
+     int area2;
+ Triangle(int d,int ba)
+ {
+  dis=d;
+  base=ba;
+ }
+
+ void areaTriangle()
+ {
+    
+     area2=dis*base;
+ }
+ void displayTrianglearea()
+ {
+     System.out.println("Triangle area "+area2);
+ }
+}
+
+
+class Area
+{
+    public static void main(String args[]) {
+        Triangle t1=new Triangle(3,4);
+        t1.areaTriangle();
+        t1.displayTrianglearea();
+        Rectangle r1=new Rectangle(4, 8);
+        r1.areaRectangle();
+        r1.displayRectanglearea();
+        
+    }
+}
