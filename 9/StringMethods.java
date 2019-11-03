d2f73486d7de97abf34ed0c1fcd28a3d00db6bf1
jermgaming <atinder999@gmail.com>
2019-11-03 13:32:03 +0530

    StringMethods

---
+++ 9/StringMethods.java
@@ -0,0 +1,10 @@
+class StringMethods
+{
+    public static void main(String args[]) {
+        String s="Hello world";
+        System.out.println(s.toLowerCase());                  //convert the string in lowercase
+        System.out.println(s.toUpperCase());                  //convert the string in uppercase
+        System.out.println(s.charAt(2));                      //return charvalue at particular index 
+    
+    }
+}
