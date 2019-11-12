43ffa56675204a9adf4762dee0cfb782260e262e
jermgaming <atinder999@gmail.com>
2019-11-12 20:23:21 +0530

    inital commit

---
+++ 8/StringSort.java
@@ -0,0 +1,21 @@
+class StringSort
+{
+     public static void main(String[] args) {
+      
+        String[] words = { "hello", "HELLO", "HellO", "hELLo" };
+        for(int i = 0; i < 3; i++) {
+            for (int j = i + 1; j < 4; j++) {
+                if (words[i].compareTo(words[j]) > 0) {
+                    // swap words[i] with words[j]
+                    String temp = words[i];
+                    words[i] = words[j];
+                    words[j] = temp;
+                }
+            }
+        }
+        System.out.println("Sorted String is:");
+        for(int i = 0; i < 4; i++) {
+            System.out.println(words[i]);
+        }
+    }
+}
